cc6526d1b7e407c7bbc1459f9284dbe92e327b7e
Matas Siaurodinas <147089390+Ghirahim5@users.noreply.github.com>
2024-02-13 22:29:43 +0200

    Add files via upload

--- v0.1.cpp
+++ v0.1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
+#include <cctype>
 #include <string>
 using namespace std;
 
@@ -17,25 +18,83 @@ int main() {
     double rez;
     string pasirinkimas;
 
+    // Vardo ivestis
+
     cout << "Koks jusu vardas?" << endl;
-    cin >> Duomenys.v;
+    bool valid_v = true;
+    do {
+        if (!(cin >> Duomenys.v)) {
+            valid_v = false;
+        }
+        else {
+            valid_v = true;
+            for (char c : Duomenys.v) {
+                if (!isalpha(c)) {
+                    valid_v = false;
+                    break;
+                }
+            }
+        }
+        if (!valid_v) {
+            cout << "Neteisinga ivestis, prasome ivesti savo varda sudaryta tik is raidziu" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while (!valid_v);
+
+    // Pavardes ivestis
+
     cout << "Kokia jusu pavarde?" << endl;
-    cin >> Duomenys.p;
+    bool valid_p = true;
+    do {
+        if (!(cin >> Duomenys.p)) {
+            valid_p = false;
+        }
+        else {
+            valid_p = true;
+            for (char c : Duomenys.p) {
+                if (!isalpha(c)) {
+                    valid_p = false;
+                    break;
+                }
+            }
+        }
+        if (!valid_p) {
+            cout << "Neteisinga ivestis, prasome ivesti savo pavarde sudaryta tik is raidziu" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while (!valid_p);
+
+    // Egzamino rezultato ivestis
+
     cout << "Koks buvo jusu egzamino rezultatas?" << endl;
     while (!(cin >> Duomenys.egz_rez) || Duomenys.egz_rez < 0 || Duomenys.egz_rez > 10) {
         cout << "Neteisinga ivestis, prasome ivesti skaiciu 10-baleje sistemoje" << endl;
-        cin.clear(); 
+        cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
+
+    // Namu darbu skaiciaus ivestis
+
     cout << "Kiek namu darbu atlikote?" << endl;
-    cin >> Duomenys.nd_sk;
+    while (!(cin >> Duomenys.nd_sk)) {
+        cout << "Neteisinga ivestis, prasome ivesti skaiciu" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
     // Namu darbu rezultatu ivedimas ir sumos skaiciavimas
 
     double nd_sum = 0;
     for (int i = 0; i < Duomenys.nd_sk; i++) {
         cout << "Koks buvo " << i + 1 << "-o namu darbo vertinimas?" << endl;
-        cin >> Duomenys.nd_rez[i];
+        while (!(cin >> Duomenys.nd_rez[i]) || Duomenys.nd_rez[i] < 0 || Duomenys.nd_rez[i] > 10) {
+            cout << "Neteisinga ivestis, prasome ivesti skaiciu 10-baleje sistemoje" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Koks buvo " << i + 1 << "-o namu darbo vertinimas?" << endl;
+        }
         nd_sum += Duomenys.nd_rez[i];
     }
 
@@ -52,10 +111,17 @@ int main() {
     else {
         mediana = Duomenys.nd_rez[Duomenys.nd_sk / 2];
     }
+    
+    // Rezultatu spausdinimas pasirenkant Vid. arba Med.
 
     cout << "Kaip norite matyti savo galutini bala? Irasykite viena is dvieju pasirinkimu: (Vid. / Med.)" << endl;
     cin >> pasirinkimas;
     cout << endl;
+
+    while (pasirinkimas != "Vid." && pasirinkimas != "Med.") {
+        cout << "Pasirinkite arba 'Vid.' arba 'Med.'" << endl;
+        cin >> pasirinkimas;
+    }
 
     if (pasirinkimas == "Vid.") {
         double nd_avg = nd_sum / Duomenys.nd_sk;
@@ -64,15 +130,14 @@ int main() {
         cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Vid.)" << endl;
         cout << "-----------------------------------------------" << endl;
         cout << left << setw(15) << Duomenys.v << setw(15) << Duomenys.p << fixed << setprecision(2) << setw(15) << rez << endl;
-
     }
     else if (pasirinkimas == "Med.") {
-
         rez = 0.4 * mediana + 0.6 * Duomenys.egz_rez;
 
         cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Med.)" << endl;
         cout << "-----------------------------------------------" << endl;
         cout << left << setw(15) << Duomenys.v << setw(15) << Duomenys.p << fixed << setprecision(2) << setw(15) << rez << endl;
     }
+
     return 0;
 }
